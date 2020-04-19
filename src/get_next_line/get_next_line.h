038aadec5f5502d25c488e6e8dee71fe5a44e832
violettagoldman <violagoldman@mail.ru>
2020-04-19 14:34:34 +0200

    Parsing

---
+++ src/get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoldman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/18 23:15:38 by vgoldman          #+#    #+#             */
+/*   Updated: 2020/04/10 15:22:29 by vgoldman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# define BUFFER_SIZE 1000
+
+int		get_next_line(int fd, char **line);
+char	*get_rest_buffer(char *str);
+int		breaks_checker(char *buffer);
+char	*get_first_line(char *buffer);
+char	*ft_strappend(char *str, char *add, int size);
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
