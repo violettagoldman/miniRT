3dbce5957b4e359ad0242181ebe8db6941804f4b
violettagoldman <violagoldman@mail.ru>
2020-05-17 12:20:36 +0200

    New minilibx_linux

--- minilibx_linux/test/Makefile.mk
+++ minilibx_linux/test/Makefile.mk
@@ -6,7 +6,7 @@ INCLIB=$(INC)/../lib
 
 CC=gcc
 
-CFLAGS= -I$(INC) -O3 -I.. -w
+CFLAGS= -I$(INC) -O3 -I..
 
 NAME= mlx-test
 SRC = main.c
@@ -15,10 +15,10 @@ OBJ = $(SRC:.c=.o)
 all	:$(NAME)
 
 $(NAME)	:$(OBJ)
-	@$(CC) -o $(NAME) $(OBJ) -L.. -lmlx -L$(INCLIB) -lXext -lX11 -lm -lbsd
+	$(CC) -o $(NAME) $(OBJ) -L.. -lmlx -L$(INCLIB) -lXext -lX11 -lm -lbsd
 
 clean	:
-	@rm -f $(NAME) $(OBJ) *~ core *.core
+	rm -f $(NAME) $(OBJ) *~ core *.core
 
 
 re	: clean all
